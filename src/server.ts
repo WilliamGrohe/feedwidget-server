391fed4bb7540e3f2b427ab9cca07f52147a0a0e
William Grohe <william.grohe@gmail.com>
2022-05-07 00:06:18 -0300

    Feedback routes and e-mail sending

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,44 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
+import { prisma } from './prisma';
 
 const app = express();
+app.use(express.json());
 
-app.post('/feedbacks', (req,res) =>{
-  return res.send('Hello World')
-})
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "4db20230389a33",
+    pass: "cf263f8751c7e7"
+  }
+});
+
+app.post('/feedbacks', async (req,res) =>{
+  const {type, comment, screenshot} = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    }
+  })
+
+  transport.sendMail({
+    from: 'Equipe FeedWid <oi@feedwid.com>',
+    to: 'William Grohe <william.grohe@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
+  })
+
+  return res.status(201).json({data: feedback})
+});
 
 
 app.listen(3333, ()=> {
