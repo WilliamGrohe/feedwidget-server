4256814658d0e4b2ff11cd7d4e148df5af0ed2a5
William Grohe <william.grohe@gmail.com>
2022-05-07 01:19:48 -0300

    applying SOLID on aplication

---
+++ src/adapter/nodemailer/nodemailerMailAdapter.ts
@@ -0,0 +1,23 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mailAdapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "4db20230389a33",
+    pass: "cf263f8751c7e7"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({subject, body}: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe FeedWid <oi@feedwid.com>',
+      to: 'William Grohe <william.grohe@gmail.com>',
+      subject,
+      html: body,
+    })
+
+  };
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,40 +1,26 @@
 import express from 'express'
-import nodemailer from 'nodemailer'
-import { prisma } from './prisma'
+import { NodemailerMailAdapter } from './adapter/nodemailer/nodemailerMailAdapter';
+import { PrismaFeedbacksRepository } from './repositories/prisma/prismaFeedbacksRepository';
+import { SubmitFeedbackUseCase } from './use-cases/submitFeedbackUseCase';
 
 export const routes = express.Router()
-
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "4db20230389a33",
-    pass: "cf263f8751c7e7"
-  }
-});
 
 routes.post('/feedbacks', async (req,res) =>{
   const {type, comment, screenshot} = req.body;
 
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot,
-    }
-  })
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+  const nodemailerMailAdapter = new NodemailerMailAdapter()
+  
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedbacksRepository,
+    nodemailerMailAdapter
+  )
 
-  transport.sendMail({
-    from: 'Equipe FeedWid <oi@feedwid.com>',
-    to: 'William Grohe <william.grohe@gmail.com>',
-    subject: 'Novo feedback',
-    html: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
-      `<p>Tipo do feedback: ${type}</p>`,
-      `<p>Comentário: ${comment}</p>`,
-      `</div>`
-    ].join('\n')
-  })
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot,
+  });
 
-  return res.status(201).json({data: feedback})
+  return res.status(201).send()
 });
